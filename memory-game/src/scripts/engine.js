fafde393c436321253bfd6face9d82ad610da737
wadi-na <wadnaevanydm@gmail.com>
2024-12-10 13:37:17 -0300

    att game memoria

--- memory-game/src/scripts/engine.js
+++ memory-game/src/scripts/engine.js
@@ -29,5 +29,31 @@ for (let i=0; i<emojis.length; i++){
 }
 
 function handleClick(){
-    
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add('boxMatch');
+        openCards[1].classList.add('boxMatch');
+    } else {
+        openCards[0].classList.remove('boxOpen');
+        openCards[1].classList.remove('boxOpen');
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        function newText(){
+            document.getElementsByTagName('h2').innerHTML = "voce venceu!"; 
+        }
+        newText();
+    };
 }
